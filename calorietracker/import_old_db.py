d3b3cfcf12d2db0726ba9591aeb6b2c31218ec5c
Patrick Speiser <mail@patrickspeiser.at>
2013-10-05 18:09:06 +0200

    Added import_old_db function

---
+++ calorietracker/import_old_db.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from calorietracker.models import Meal, Food
+import sqlite3
+
+
+def import_old_db(request):
+    delete_current_db()
+    conn = sqlite3.connect("C:/Users/Doskir/Dropbox/CalorieTracker/caloriedb.s3db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM food")
+    foods = c.fetchall()
+    for food in foods:
+        f = Food(id=food[0], name=food[1], kcal_per_100g=food[2], grams=food[3], hidden=bool(food[4]))
+        f.save()
+    c.execute("SELECT * FROM meals")
+    meals = c.fetchall()
+    for meal in meals:
+        m = Meal(id=meal[0], food=Food.objects.get(pk=meal[1]), date=meal[2])
+        m.save()
+    c.close()
+    conn.close()
+    return HttpResponse(str(meals))
+
+
+def delete_current_db():
+    for meal in Meal.objects.all():
+        meal.delete()
+    for food in Food.objects.all():
+        food.delete()
--- calorietracker/urls.py
+++ calorietracker/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, url
 
-from calorietracker import views
+from calorietracker import views, import_old_db
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^test.json',views.test_json,name='test.json'),
-    url(r'^meals.json',views.meals_json,name='meals.json'),
-    url(r'^weeks.json',views.weeks_json,name='weeks.json'),
-    url(r'd3js.json',views.d3js_json,name='d3js.json'),
-    url(r'jstree.json',views.jstree_json,name='jstree.json'),
-
+                       url(r'^$', views.index, name='index'),
+                       url(r'^test.json', views.test_json, name='test.json'),
+                       url(r'^meals.json', views.meals_json, name='meals.json'),
+                       url(r'^weeks.json', views.weeks_json, name='weeks.json'),
+                       url(r'd3js.json', views.d3js_json, name='d3js.json'),
+                       url(r'jstree.json', views.jstree_json, name='jstree.json'),
+                       url(r'import_old_db', import_old_db.import_old_db, name='import_old_db')
 
 
 )
